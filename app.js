415a79daa1e94810df8b3f887c8f3b0cc76883cf
Naveen1422github <76592102+Naveen1422github@users.noreply.github.com>
2023-05-17 14:41:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
+  port: '3306',
   user: 'root',
   password: '#Np@1938',
   database: 'npdb'
