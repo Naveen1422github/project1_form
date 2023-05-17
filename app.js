0b31cb9da06bc024635deacea0ebb0bcce33e83a
Naveen1422github <76592102+Naveen1422github@users.noreply.github.com>
2023-05-17 14:44:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.post('/submit', (req, res) => {
 });
 
 // Start the server
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server started on port', process.env.PORT || 3000);
+app.listen(process.env.PORT || 3306=> {
+  console.log('Server started on port', process.env.PORT || 3306);
 });
 
